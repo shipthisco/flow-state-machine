9258694a63c70bbf91d6ff976b46e12c2531d2cd
mayurrawte <mayur@shipthis.co>
2026-02-18 17:26:29 +0530

    State machine rendered

--- src/web-component.jsx
+++ src/web-component.jsx
@@ -3,11 +3,45 @@ import { createRoot } from 'react-dom/client'
 import StatesCanvas from './StatesCanvas'
 import './index.css'
 
+function computeJsonDiff(oldText, newText) {
+  const oldLines = oldText.split('\n')
+  const newLines = newText.split('\n')
+
+  const m = oldLines.length
+  const n = newLines.length
+  const dp = Array.from({ length: m + 1 }, () => new Array(n + 1).fill(0))
+  for (let i = 1; i <= m; i++) {
+    for (let j = 1; j <= n; j++) {
+      dp[i][j] = oldLines[i - 1] === newLines[j - 1]
+        ? dp[i - 1][j - 1] + 1
+        : Math.max(dp[i - 1][j], dp[i][j - 1])
+    }
+  }
+
+  const stack = []
+  let i = m, j = n
+  while (i > 0 || j > 0) {
+    if (i > 0 && j > 0 && oldLines[i - 1] === newLines[j - 1]) {
+      stack.push({ type: 'same', text: ' ' + oldLines[i - 1] })
+      i--; j--
+    } else if (j > 0 && (i === 0 || dp[i][j - 1] >= dp[i - 1][j])) {
+      stack.push({ type: 'add', text: '+' + newLines[j - 1] })
+      j--
+    } else {
+      stack.push({ type: 'remove', text: '-' + oldLines[i - 1] })
+      i--
+    }
+  }
+  stack.reverse()
+  return stack
+}
+
 class WorkflowStatesCanvasElement extends HTMLElement {
   constructor() {
     super()
     this._root = null
     this._workflow = null
+    this._currentWorkflow = null
     this._apiConfig = {
       apiEndpoint: '',
       authToken: '',
@@ -23,6 +57,11 @@ class WorkflowStatesCanvasElement extends HTMLElement {
   }
 
   connectedCallback() {
+    // Stop keyboard events from bubbling out to Angular's zone.js
+    const stop = (e) => e.stopPropagation()
+    for (const evt of ['keydown', 'keypress', 'keyup']) {
+      this.addEventListener(evt, stop)
+    }
     this._root = createRoot(this)
     this._render()
   }
@@ -34,18 +73,19 @@ class WorkflowStatesCanvasElement extends HTMLElement {
     }
   }
 
-  // --- workflow ---
+  // --- workflow (set to initialize/reset, get to read current edited state) ---
 
   set workflow(value) {
     this._workflow = value || null
+    this._currentWorkflow = value ? JSON.parse(JSON.stringify(value)) : null
     this._render()
   }
 
   get workflow() {
-    return this._workflow
+    return this._currentWorkflow
   }
 
-  // --- apiConfig (for live API fetching) ---
+  // --- apiConfig ---
 
   set apiConfig(value) {
     this._apiConfig = { ...this._apiConfig, ...(value || {}) }
@@ -56,7 +96,7 @@ class WorkflowStatesCanvasElement extends HTMLElement {
     return { ...this._apiConfig }
   }
 
-  // --- roles (direct data) ---
+  // --- roles ---
 
   set roles(value) {
     this._roles = value || null
@@ -67,7 +107,7 @@ class WorkflowStatesCanvasElement extends HTMLElement {
     return this._roles
   }
 
-  // --- actionEvents (direct data) ---
+  // --- actionEvents ---
 
   set actionEvents(value) {
     this._actionEvents = value || null
@@ -78,7 +118,7 @@ class WorkflowStatesCanvasElement extends HTMLElement {
     return this._actionEvents
   }
 
-  // --- fieldMeta (direct data) ---
+  // --- fieldMeta ---
 
   set fieldMeta(value) {
     this._fieldMeta = value || null
@@ -87,6 +127,12 @@ class WorkflowStatesCanvasElement extends HTMLElement {
 
   get fieldMeta() {
     return this._fieldMeta
+  }
+
+  getDiff() {
+    const original = JSON.stringify(this._workflow, null, 2)
+    const current = JSON.stringify(this._currentWorkflow, null, 2)
+    return computeJsonDiff(original, current)
   }
 
   _render() {
@@ -99,13 +145,7 @@ class WorkflowStatesCanvasElement extends HTMLElement {
         actionEvents={this._actionEvents}
         fieldMeta={this._fieldMeta}
         onChange={(updatedWorkflow) => {
-          this._workflow = updatedWorkflow
-          this.dispatchEvent(
-            new CustomEvent('workflow-change', {
-              detail: updatedWorkflow,
-              bubbles: true,
-            })
-          )
+          this._currentWorkflow = updatedWorkflow
         }}
       />
     )
